6a1af9d434f7833d96fa34450b69fc87fbe52734
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-10-26 06:20:49 +0100

    Clean-up template

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React starter template</h1>
+        <p>Since React 17, you don&#39;t need this anymore:</p>
+        <pre>
+          <code>import React from &#39;react&#39;;</code>
+        </pre>
       </header>
     </div>
   );
